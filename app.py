768f1832bc046f52e8ccd4490c4386a3696aef9a
Marcos Diaz <marcos.dure.diaz@gmail.com>
2023-06-18 03:07:14 -0300

    get playlists works

--- app.py
+++ app.py
@@ -3,42 +3,80 @@ import common
 import acess_token
 import secrets
 import string
-from flask import Flask, redirect
+from flask import Flask, redirect, request, session, abort, url_for
 from urllib import parse
+import base64
 
 app = Flask("spotify_automation")
 
+ALPHABET = string.ascii_letters + string.digits
+REDIRECT_URI = "{}:{}/{}".format(common.APP_BASE_ADRESS,
+                                 str(common.APP_PORT), 'callback')
+
+
+def make_acess_token_request(auth_code: str):
+    req_url = common.SPOTIFY_ACCOUNT_BASE_ADDRESS + common.SPOTIFY_ACESS_TOKEN_ADDRESS
+    req_headers = {"content-type": "application/x-www-form-urlencoded",
+                   "Authorization": "Basic " + base64.b64encode((common.SPOTIFY_CLIENT_ID + ":" + common.SPOTIFY_CLIENT_SECRET).encode("ascii")).decode('ascii')}
+
+    body_params = {"grant_type": "authorization_code",
+                   "code": auth_code,
+                   "redirect_uri": REDIRECT_URI}
+
+    response = requests.post(req_url, headers=req_headers, data=body_params)
+
+    return response
+
+
 @app.route("/login")
 def request_user_auth():
-    alphabet = string.ascii_letters + string.digits
     SCOPE = "playlist-read-private playlist-modify-private playlist-modify-public"
-    redirect_uri = "{}:{}/{}".format(common.APP_BASE_ADRESS,str(common.APP_PORT),'callback')
+
+    # store the request state on the client side to protect against XSRF
+    state_secret = ''.join(secrets.choice(ALPHABET) for i in range(16))
+    session["state_secret"] = state_secret
 
     req_url = common.SPOTIFY_ACCOUNT_BASE_ADDRESS + "/authorize"
+
     req_params = {"client_id": common.SPOTIFY_CLIENT_ID,
                   "response_type": "code",
                   "scope": SCOPE,
-                  "redirect_uri": redirect_uri,
-                  "state": ''.join(secrets.choice(alphabet) for i in range(16))}
+                  "redirect_uri": REDIRECT_URI,
+                  "state": state_secret}
 
-    return redirect(req_url + "?" + parse.urlencode(req_params))
+    req_url_with_params = req_url + "?" + parse.urlencode(req_params)
+    return redirect(req_url_with_params)
+
 
 @app.route("/callback")
 def request_access_token():
-    return "<p>Hello, World!</p>"
+    # get the auth code from the spotify auth server
+    state = request.args.get("state")
+    auth_code = request.args.get("code")
+    error = request.args.get("error")
+
+    if (state != session["state_secret"]):
+        abort(401)
+
+    if (error != None):
+        return error
+
+    res = make_acess_token_request(auth_code)
+    session["access_token"] = res.json()["access_token"]
+    # return res.json()
+    return redirect(url_for('get_playlists'))
+
+
+@app.route("/playlists")
+def get_playlists():
+    req_url = common.SPOTIFY_API_BASE_ADRESS + \
+        common.SPOTIFY_MY_PLAYLISTS_ADDRESS
+    req_headers = {"Authorization": "Bearer " + session["access_token"]}
+    res = requests.get(req_url, headers=req_headers)
+    return res.json()
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=common.APP_PORT, host="localhost")
-
-# get my user id
-# this doesnt work, we first need to use the oauth authotization flow so my user grants my app authorization
-# then my app gets another token
-
-
-# def get_user_id():
-#     req_url = common.API_BASE_ADRESS+"/me"
-#     req_headers = {'Authorization': 'Bearer ' + acess_token.get_access_token()}
-#     return requests.get(common.API_BASE_ADRESS+"/me", headers=req_headers).json()
-
-
-#print(get_user_id())
+    # secret to use sessions
+    app.secret_key = secrets.token_hex().encode()
+    app.run(debug=True, port=common.APP_PORT, host=common.APP_HOST)
--- common.py
+++ common.py
@@ -1,6 +1,15 @@
+import os
+import spotify_secrets
+
 APP_PORT = 8080
 APP_BASE_ADRESS = "http://localhost"
+APP_HOST = "localhost"
 
 SPOTIFY_API_BASE_ADRESS = "https://api.spotify.com/v1"
 SPOTIFY_ACCOUNT_BASE_ADDRESS = "https://accounts.spotify.com"
-SPOTIFY_CLIENT_ID = "fa4954c77aad4069b8cb0833a79f5eb6"
+# SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
+# SPOTIFY_CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
+SPOTIFY_CLIENT_ID = spotify_secrets.SPOTIFY_CLIENT_ID
+SPOTIFY_CLIENT_SECRET = spotify_secrets.SPOTIFY_CLIENT_SECRET
+SPOTIFY_ACESS_TOKEN_ADDRESS = "/api/token"
+SPOTIFY_MY_PLAYLISTS_ADDRESS = "/me/playlists"
