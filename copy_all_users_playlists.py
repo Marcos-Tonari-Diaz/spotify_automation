29841f1a4783c69430c72b728bbeae2fa253a9ba
Marcos Diaz <marcos.dure.diaz@gmail.com>
2023-11-19 19:43:32 -0300

    tested copy_all_users

--- copy_all_users_playlists.py
+++ copy_all_users_playlists.py
@@ -3,8 +3,13 @@ from repository import RepositoryFactory
 
 
 def lambda_handler(event, context):
+    # def copy_all_users():
     repo = RepositoryFactory.create_repository()
     for user in repo.get_all_users():
         user_id = user["UserSpotifyId"]
         copier = copy_script.Copier(user_id)
         copier.copy_discoverweekly_to_archive()
+
+
+# if __name__ == "__main__":
+#     copy_all_users()
